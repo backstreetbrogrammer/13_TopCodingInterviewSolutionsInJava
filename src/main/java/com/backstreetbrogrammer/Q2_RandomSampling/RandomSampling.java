43b86649b18b6403855289871f7cf5e7c5abd9ff
Rishi Srivastava <rishisrivasta@gmail.com>
2023-02-14 08:35:34 +0800

    refactored problem 2 on sampling

--- src/main/java/com/backstreetbrogrammer/Q2_RandomSampling/RandomSampling.java
+++ src/main/java/com/backstreetbrogrammer/Q2_RandomSampling/RandomSampling.java
@@ -1,14 +1,30 @@
 package com.backstreetbrogrammer.Q2_RandomSampling;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomSampling {
 
-    public static List<Integer> randomSampling(final List<Integer> origData, final int sampleSize) {
-        final List<Integer> data = new ArrayList<>(List.copyOf(origData));
+    private final List<Integer> data;
+    private final int sampleSize;
+
+    public RandomSampling(final List<Integer> data, final int sampleSize) {
+        this.data = data;
+        this.sampleSize = sampleSize;
+    }
+
+    public List<Integer> randomSamplingUsingShuffle(final Random random) {
+        if (random == null) {
+            Collections.shuffle(data);
+        } else {
+            Collections.shuffle(data, random);
+        }
+        return data.subList(0, sampleSize);
+    }
+
+    public List<Integer> randomSamplingUsingSwap() {
         for (int i = 0; i < sampleSize; i++) {
             Collections.swap(data, i, i + ThreadLocalRandom.current().nextInt(data.size() - i));
         }
--- src/test/java/com/backstreetbrogrammer/Q2_RandomSampling/RandomSamplingTest.java
+++ src/test/java/com/backstreetbrogrammer/Q2_RandomSampling/RandomSamplingTest.java
@@ -1,24 +1,57 @@
 package com.backstreetbrogrammer.Q2_RandomSampling;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class RandomSamplingTest {
 
-    @Test
-    void testRandomSampling() {
-        final var data = Stream.iterate(1, n -> n + 1)
-                               .limit(10L)
-                               .collect(Collectors.toList());
-        final var sampleSize = 4;
+    private final List<Integer> data = Stream.iterate(1, n -> n + 1)
+                                             .limit(10L)
+                                             .collect(Collectors.toList());
+    private final int sampleSize = 4;
+
+    private final RandomSampling sampling = new RandomSampling(data, sampleSize);
+
+    @BeforeEach
+    void setUp() {
         System.out.println("Original list: " + data);
         System.out.printf("----- Samples of size %d: %n", sampleSize);
+    }
+
+    @Test
+    @DisplayName("Random sampling using Collections.shuffle method")
+    void testRandomSamplingUsingCollectionsShuffle() {
         for (int i = 0; i < 5; i++) {
-            RandomSampling.randomSampling(data, sampleSize)
-                          .forEach(j -> System.out.printf("%d, ", j));
+            sampling.randomSamplingUsingShuffle(null)
+                    .forEach(j -> System.out.printf("%d, ", j));
             System.out.println();
         }
     }
+
+    @Test
+    @DisplayName("Random sampling using Collections.shuffle method with ThreadLocalRandom")
+    void testRandomSamplingUsingCollectionsShuffleWithGivenRandom() {
+        for (int i = 0; i < 5; i++) {
+            sampling.randomSamplingUsingShuffle(ThreadLocalRandom.current())
+                    .forEach(j -> System.out.printf("%d, ", j));
+            System.out.println();
+        }
+    }
+
+    @Test
+    @DisplayName("Random sampling using Collections.swap method")
+    void testRandomSamplingUsingCollectionsSwap() {
+        for (int i = 0; i < 5; i++) {
+            sampling.randomSamplingUsingSwap()
+                    .forEach(j -> System.out.printf("%d, ", j));
+            System.out.println();
+        }
+    }
+
 }
