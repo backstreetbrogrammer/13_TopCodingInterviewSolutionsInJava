1f5035dc2202ff8acf4a611cde53a4cd0c6d2ed3
Rishi Srivastava <rishisrivasta@gmail.com>
2023-02-21 07:17:17 +0800

    added unit test case for JTail

--- src/test/java/com/backstreetbrogrammer/Q6_UnixTail/JTailTest.java
+++ src/test/java/com/backstreetbrogrammer/Q6_UnixTail/JTailTest.java
@@ -1,4 +1,28 @@
 package com.backstreetbrogrammer.Q6_UnixTail;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
 public class JTailTest {
+
+    @ParameterizedTest
+    @MethodSource("getJTailObjects")
+    @DisplayName("Test tail method for a given file")
+    void testTailMethodForAGivenFile(final JTail jTail) throws IOException {
+        System.out.println(jTail.tail());
+        System.out.println("-----------------\n");
+    }
+
+    private static Stream<Arguments> getJTailObjects() {
+        final String fileName = "src\\test\\resources\\amendments.txt";
+        return Stream.of(
+                Arguments.of(new JTail(fileName, 2)),
+                Arguments.of(new JTail(fileName, 3)),
+                Arguments.of(new JTail(fileName, 5)));
+    }
 }
