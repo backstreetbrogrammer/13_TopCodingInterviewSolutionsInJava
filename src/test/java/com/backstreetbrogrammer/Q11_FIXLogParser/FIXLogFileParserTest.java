bf2d434fa7181698990518a5a6dea44da417333e
Rishi Srivastava <rishisrivasta@gmail.com>
2023-10-30 07:44:20 +0800

    new test cases for FIX log

--- src/test/java/com/backstreetbrogrammer/Q11_FIXLogParser/FIXLogFileParserTest.java
+++ src/test/java/com/backstreetbrogrammer/Q11_FIXLogParser/FIXLogFileParserTest.java
@@ -5,10 +5,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.channels.SeekableByteChannel;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -123,11 +124,49 @@ public class FIXLogFileParserTest {
         assertFalse(fixTagValueMap.isEmpty());
     }
 
+    @Test
+    void testEfficientWriteToALargeFile() throws IOException {
+        final Path writeFilePath = Path.of("src", "main", "resources", "fix_log3.txt");
+        final Path readFilePath = Path.of("src", "main", "resources", "fix_log1.txt");
+        try (
+                final RandomAccessFile randomAccessFile = new RandomAccessFile(writeFilePath.toFile(), "rws");
+                final FileChannel fileChannel = randomAccessFile.getChannel()
+        ) {
+            try (final BufferedReader fileReader = Files.newBufferedReader(readFilePath)) {
+                String line;
+                while ((line = fileReader.readLine()) != null) {
+                    line += System.lineSeparator();
+                    final byte[] lineInBytes = line.getBytes(StandardCharsets.UTF_8);
+                    final ByteBuffer buffer = ByteBuffer.allocate(lineInBytes.length);
+                    buffer.put(lineInBytes);
+                    buffer.flip();
+
+                    final int bytesWritten = fileChannel.write(buffer);
+                    assertTrue(bytesWritten > 0);
+                }
+            }
+        }
+    }
+
+    @Test
+    void testEasiestWayToWriteToAFile() throws IOException {
+        final Path writeFilePath = Path.of("src", "main", "resources", "fix_log3.txt");
+        final Path readFilePath = Path.of("src", "main", "resources", "fix_log1.txt");
+        try (
+                final BufferedReader reader = Files.newBufferedReader(readFilePath);
+                final BufferedWriter writer = Files.newBufferedWriter(writeFilePath)
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.newLine();
+            }
+        }
+    }
+
     private static Stream<Arguments> getFixLogLine() {
         return Stream.of(
-                Arguments.of("8=FIX.4.2 | 9=178 | 35=8 | 49=PHLX | 56=PERS | 52=20071123-05:30:00.000 | " +
-                                     "11=ATOMNOCCC9990900 | 20=3 | 150=E | 39=E | 55=MSFT | 167=CS | 54=1 | 38=15 | " +
-                                     "40=2 | 44=15 | 58=PHLX EQUITY TESTING | 59=0 | 47=C | 32=0 | 31=0 | 151=15 | 14=0 | 6=0 | 10=128 |",
+                Arguments.of("8=FIX.4.2 | 9=178 | 35=8 | 49=PHLX | 56=PERS | 52=20071123-05:30:00.000 | 11=ATOMNOCCC9990900 | 20=3 | 150=E | 39=E | 55=MSFT | 167=CS | 54=1 | 38=15 | 40=2 | 44=15 | 58=PHLX EQUITY TESTING | 59=0 | 47=C | 32=0 | 31=0 | 151=15 | 14=0 | 6=0 | 10=128 |",
                              "\\b(\\d+)=([^\\u007c]+)"),
                 Arguments.of("8=FIX.4.2   9=178   35=8   49=PHLX   56=PERS   52=20071123-05:30:00.000   11=ATOMNOCCC9990900   20=3   150=E   39=E   55=MSFT   167=CS   54=1   38=15   40=2   44=15   58=PHLX EQUITY TESTING   59=0   47=C   32=0   31=0   151=15   14=0   6=0   10=128  ",
                              "\\b(\\d+)=([^\\s]+)")
